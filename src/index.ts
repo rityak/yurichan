2394d0b13f96ef9f366160c773df18790a729ff1
Kirill Katelyanets <katelyanets@gmail.com>
2023-01-14 19:25:37 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf } from 'telegraf'
 
-import { execute } from './execute.js';
-import { generatedMessageHandlers } from './yuri/generatedMessageHandlers.js';
-import chatSuccess from './yuri/chatSuccess.js';
+import { execute } from './execute.js'
+import { generatedMessageHandlers } from './yuri/generatedMessageHandlers.js'
+import chatSuccess from './yuri/chatSuccess.js'
 
 await execute(() => {
-  const USE_IN_CHATS = ['-1001839637577'];
-  const PRODUCTION = process.env?.PRODUCTION === 'true';
-  const TOKEN = process.env?.TOKEN || '';
-  const bot: Telegraf = new Telegraf(TOKEN);
+  const USE_IN_CHATS = ['-1001839637577']
+  const PRODUCTION = process.env?.PRODUCTION === 'true'
+  const TOKEN = process.env?.TOKEN || ''
+  const bot: Telegraf = new Telegraf(TOKEN)
 
   if (PRODUCTION) {
-    USE_IN_CHATS.push('-1001635567203');
+    USE_IN_CHATS.push('-1001635567203')
   }
 
-  chatSuccess(bot, USE_IN_CHATS);
-  generatedMessageHandlers(bot);
+  chatSuccess(bot, USE_IN_CHATS)
+  generatedMessageHandlers(bot)
 
   bot
     .launch()
-    .then(() => {})
-    .catch(console.error);
+    .then(() => {
+      console.log('test')
+    })
+    .catch(console.error)
 
-  return bot;
-});
+  return bot
+})
