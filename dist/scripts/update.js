5ed51eed7b11b6e1d916b9f3b67a7e662dc3ab2a
Kirill Katelyanets <katelyanets@gmail.com>
2023-03-30 19:11:12 +0800

    update

--- dist/scripts/update.js
+++ dist/scripts/update.js
@@ -1,26 +1,15 @@
-import { exec } from 'node:child_process';
+import { $ } from 'zx';
 import isAdmin from '../yuri/utils/isAdmin.js';
 export default function updateBot(bot) {
     bot.command('update', context => {
         const user_id = context?.update?.message?.from?.id.toString();
         const success = isAdmin(user_id);
         setTimeout(async () => {
-            console.log(success);
             if (success) {
                 bot.stop();
-                exec('npm run start', (error, stdout, stderr) => {
-                    if (error) {
-                        console.log(`error: ${error.message}`);
-                        return;
-                    }
-                    if (stderr) {
-                        console.log(`stderr: ${stderr}`);
-                        return;
-                    }
-                    console.log(`stdout: ${stdout}`);
-                });
+                await $ `npm run start`;
             }
-        }, 100);
+        }, 1000);
     });
 }
 //# sourceMappingURL=update.js.map
